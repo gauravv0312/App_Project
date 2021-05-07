abaf07154f58235776e83c25916d2b8f1225ca76
Gaurav Verma <70060764+gauravv0312@users.noreply.github.com>
2021-05-07 13:41:21 +0530

    Add some new features

--- Application/app/src/main/java/com/example/application/MainActivity.java
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -4,19 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    EditText address,Fullname,number;
     Button start;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        address=findViewById(R.id.address);
+        Fullname=findViewById(R.id.Fullname);
+        number=findViewById(R.id.number);
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+        userId=fAuth.getCurrentUser().getUid();
+        DocumentReference documentReference=fStore.collection("User").document(userId);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                  Fullname.setText(documentSnapshot.getString("fname"));
+                  address.setText(documentSnapshot.getString("email"));
+                  number.setText(documentSnapshot.getString("phone"));
+            }
+        });
     }
     public void start(View view){
         startActivity(new Intent(getApplicationContext(),register.class));
